4abb3c7018c4c1aeaefd99de534219b03a97c573
Roshan Singh <roshan2152@gmail.com>
2025-02-13 13:16:14 +0530

    ashbfkfa

--- hello-world-api/Startup.cs
+++ hello-world-api/Startup.cs
@@ -1,13 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Threading.Tasks;
+// using Microsoft.AspNetCore.Builder;
+// using Microsoft.AspNetCore.Hosting;
+// using Microsoft.Extensions.Configuration;
+// using Microsoft.Extensions.DependencyInjection;
+// using Microsoft.Extensions.Logging;
+// using Microsoft.Extensions.Options;
+
+// namespace hello_world_api
+// {
+//     public class Startup
+//     {
+//         public Startup(IConfiguration configuration)
+//         {
+//             Configuration = configuration;
+//         }
+
+//         public IConfiguration Configuration { get; }
+
+//         // This method gets called by the runtime. Use this method to add services to the container.
+//         public void ConfigureServices(IServiceCollection services)
+//         {
+//             services.AddMvc();
+//         }
+
+//         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+//         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+//         {
+//             if (env.IsDevelopment())
+//             {
+//                 app.UseDeveloperExceptionPage();
+//             }
+
+//             app.UseMvc();
+//         }
+//     }
+// }
+
+
+using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting; // ✅ Required for IsDevelopment()
+
 
 namespace hello_world_api
 {
@@ -23,18 +63,25 @@ namespace hello_world_api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddControllers(); // Use this instead of AddMvc()
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) // Note: IWebHostEnvironment in .NET 3+
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseMvc();
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers(); // Maps API controllers automatically
+            });
         }
     }
 }
